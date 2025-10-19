67ba384b6eb644926acfc1cb0e6d294d48bf703d
Ilyas <weendibol21300@gmail.com>
2025-10-19 20:12:42 +0500

    Add button examples to playground: showcase UIButton component with various severity and variant props in app.tsx.

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,8 +1,33 @@
 import { defineComponent } from 'vue'
 
+import { UIButton } from '@murrui/core'
+
 export default defineComponent(() => {
   return () => (
     <>
+      <div class="flex flex-col gap-2">
+        <div class="inline-flex flex-col gap-2 p-4">
+          <h1 class="">Button severity: default 'Primary'</h1>
+          <div class="flex flex-nowrap items-center gap-2">
+            <UIButton label='Submit'/>
+            <UIButton label='Submit' severity='secondary'/>
+            <UIButton label='Submit' severity='success'/>
+            <UIButton label='Submit' severity='info'/>
+            <UIButton label='Submit' severity='warn'/>
+            <UIButton label='Submit' severity='help'/>
+            <UIButton label='Submit' severity='danger'/>
+          </div>
+        </div>
+
+        <div class="inline-flex flex-col gap-2 p-4">
+          <h1 class="">Button variant: default 'Default'</h1>
+          <div class="flex flex-nowrap items-center gap-2">
+            <UIButton label='Submit'/>
+            <UIButton label='Submit' variant='text'/>
+            <UIButton label='Submit' variant='outlined'/>
+          </div>
+        </div>
+      </div>
     </>
   )
 })
