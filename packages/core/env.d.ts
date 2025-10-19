d8dcd5d240a6582de4ee4ce1a7f48e4161fcbdab
Ilyas <weendibol21300@gmail.com>
2025-10-19 11:31:31 +0500

    Update TypeScript configuration and package structure for murrui-core. Add JSX support in env.d.ts, adjust type definitions path in package.json, and enhance build scripts. Introduce tsconfig.build.json for declaration file generation and modify main.style.css to include Tailwind CSS with a prefix.

--- packages/core/env.d.ts
+++ packages/core/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+import { JSX } from 'vue/jsx-runtime'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends JSX.IntrinsicElements {}
+  }
+}
